cccd4738d04421996800077f323c3ceb17f4dae6
ccolon2 <65677366+ccolon2@users.noreply.github.com>
2021-11-30 02:28:34 -0500

    Update question2.c

--- question2.c
+++ question2.c
@@ -1,134 +1,155 @@
 #include <stdio.h>
-
-#include <stdint.h>
 #include <math.h>
-
 
 #define XSIZE 100
 #define YSIZE 100
+#define CACHE_SIZE 2048
+
+unsigned long int bit_extractor(unsigned long int number, int sz, int start) {
+    return (((1 << sz) - 1) & (number >> (start - 1)));
+}
+
 int main(void) {
-uint32_t tag_mask1       =   0xffff8000;
-  uint32_t index_mask1   =   0x00007fe0;
-  uint32_t index_mask2   =   0x0000ffe0;
-  uint32_t tag_mask2     =   0xfffe0000;
-  uint32_t addr;
-  uint32_t index;
-  uint32_t tag;
+  int index_length = 11;   //32 byte cache : 10   128 byte cache: 11
+  int index_start = 7;   //32 byte cache: 6    128 byte cache: 7
+  int tag_length = 15;     //32 byte cache: 17   128 byte cache: 15
+  int tag_start = 18;    //32 byte cache: 16   128 byte cache: 18
 
   int x[XSIZE][YSIZE];
   int y[XSIZE][YSIZE];
   int z[XSIZE][YSIZE];
+
   int i, j, k;
-  int hits1   = 0;
-  int hits2   = 0;
-  int misses1 = 0;
-  int misses2 = 0;
 
   int r;
-  int * temp; 
+
+  int valid_bits[CACHE_SIZE];
+  int tag[CACHE_SIZE];
+
+
 
   /* Initialize x matrix */
-  for (i = 0; i < XSIZE; i++) { 
-    for (j = 0; j < YSIZE; j++) { 
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
       x[i][j] = 0;
     }
   }
+
+
   /* Initialize y matrix */
-  for (i = 0; i < XSIZE; i++) {
-    for (j = 0; j < YSIZE; j++) {
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
       y[i][j] = i + j;
     }
   }
+
   /* Initialize z matrix */
-  for (i = 0; i < XSIZE; i++) {
-    for (j = 0; j < YSIZE; j++) {
-      z[i][j] = i + j;
+  for(i=0; i<XSIZE; i++) {
+    for(j=0; j<YSIZE; j++) {
+        z[i][j] = i + j;
     }
   }
 
+  for(i=0; i<CACHE_SIZE; i++) {
+    valid_bits[i] = 0;
+  }
+
+  for(i=0; i<CACHE_SIZE; i++) {
+    tag[i] = 0;
+  }
+  unsigned long int x_beginning = bit_extractor(&x[0][0], index_length, index_start);
+  unsigned long int y_beginning = bit_extractor(&y[0][0], index_length, index_start);
+  unsigned long int z_beginning = bit_extractor(&z[0][0], index_length, index_start);
+
+  //15 11 6
+//0000 0000 0000 000|0  0000 0000 00|00 0000
+
+//17 10 5
+//0000 0000 0000 0000  0|000 0000 000|0 0000
+  int miss = 0;
+  int hits = 0;
 
 
-  double miss_rate1,miss_rate2;
 
-  int size1 = pow(2,10); //2^10 elements in teh array
-  /******************************/
-  int valid1[size1];
-  uint32_t tags1[size1];
-/********************************/
 
-  int size2 = pow(2,11);//2^11 elements in the array
-  int valid2[size2];
-  uint32_t tags2[size2];
+  unsigned long int index_extracted;
+  unsigned long int tag_extracted;
+
+
+ 
+
+
+
   /* Do matrix multiply */
-  for (i = 0; i < XSIZE; i = i + 1) {
-    for (j = 0; j < YSIZE; j = j + 1) {
-      r = 0;
-      for (k = 0; k < XSIZE; k = k + 1) {
-        r = r + y[i][k] * z[k][j];
-      }
-      x[i][j] = r;
-      //1. Everytime you reference x, y, or z you get the addr of that array element 
-      temp = &x[i][j];  //gives me the addr of a given array element
-      addr = (uint32_t) temp;
+  for(i=0; i<XSIZE; i=i+1) {
+    for(j=0; j<YSIZE; j=j+1) {
+      // r = 0;
+      for(k=0; k<XSIZE; k=k+1) {
+        x[i][j] = x[i][j] + y[i][k] * z[k][j];
+        index_extracted = bit_extractor(&x[i][j], index_length, index_start);
+        tag_extracted = bit_extractor(&x[i][j], tag_length, tag_start);
+        //for all of X array cache
+        if(valid_bits[x_beginning - index_extracted] == 1) { // if the valid bit is a 1, compare tags 
+            if(tag[x_beginning - index_extracted] == tag_extracted) { // if data is in the cache block that has the same tag
+                hits++; //due to spatial locality 
+            }
+            else {
+                tag[x_beginning - index_extracted] = tag_extracted;//have to replace tag with updated tag
+                miss++;
+            }
+        }
+        else { //data not already in cache
+            miss++; //compulsory type miss since this is the first time you set the valid bit
+            valid_bits[x_beginning - index_extracted] = 1;//flip valid bit to 1
+            tag[x_beginning - index_extracted] = tag_extracted;//copy tag of addr into tag entry
+        }
 
-      //2. Break down the addr into tag, index, and offset fields
- 
-      index = ((addr & index_mask1) >> 5); //isolate index and shift
+        tag_extracted = bit_extractor(&y[i][k], tag_length, tag_start);
+        index_extracted = bit_extractor(&y[i][k], index_length, index_start);
+      
   
-
- 
-      tag = ((addr & tag_mask1) >> 15);//isolate tag and shift
-
-      
-
-
-      //Account for all misses and hits in Case 1
-      if (valid1[index] == 1) {// if the valid bit is a 1, compare tags 
-        if (tags1[index] == tag) { // if data is in the cache block that has the same tag
-            hits1++; //due to spatial locality 
+        if(valid_bits[y_beginning - index_extracted] == 1) {
+            if(tag[y_beginning - index_extracted] == tag_extracted) {
+                hits++;
+            }
+            else {
+                tag[y_beginning - index_extracted] = tag_extracted;
+                miss++;
+            }
         }
-        else { //tags are not the same
-            misses1++; 
-            tags1[index] = tag; //have to replace tag with updated tag
+        else {
+            miss++;
+            valid_bits[y_beginning - index_extracted] = 1;
+            tag[y_beginning - index_extracted] = tag_extracted;
         }
-
-      }
-      else {  //data not already in cache
-            valid1[index] = 1; //flip valid bit to 1
-            tags1[index] = tag; //copy tag of addr into tag entry
-            misses1++; //compulsory type miss since this is the first time you set the valid bit
-      }
-      
-      
+        tag_extracted = bit_extractor(&z[k][j], tag_length, tag_start);
+        index_extracted = bit_extractor(&z[k][j], index_length, index_start);
      
-
-      index = ((addr & index_mask2) >> 6);
-      tag = ((addr & tag_mask2) >> 17);
-      //Account for all misses and hits in Case 2
-      if (valid2[index] == 1) {// if the valid bit is a 1, compare tags 
-        if (tags2[index] == tag) { // if data is in the cache block that has the same tag
-            hits2++; //due to spatial locality 
+        if(valid_bits[z_beginning - index_extracted] == 1) {
+            if(tag[z_beginning - index_extracted] == tag_extracted) {
+                hits++;
+            }
+            else {
+                tag[z_beginning - index_extracted] = tag_extracted;
+                miss++;
+            }
         }
-        else { //tags are not the same
-            misses2++; 
-            tags2[index] = tag; //have to replace tag with updated tag
+        else {
+            miss++;
+            valid_bits[z_beginning - index_extracted] = 1;
+            tag[z_beginning - index_extracted] = tag_extracted;
         }
-
       }
-      else {  //data not already in cache
-            valid2[index] = 1; //flip valid bit to 1
-            tags2[index] = tag; //copy tag of addr into tag entry
-            misses2++; //compulsory type miss since this is the first time you set the valid bit
-      }
-      
+      // x[i][j] = r;
     }
   }
-  int total1 = misses1 + hits1;
-  int total2 = misses2 + hits2;
-  miss_rate1 = (misses1 / (double)(total1)) * 100;
-  miss_rate2 = (misses2 / (double)(total2)) * 100;
-  printf("first case: %.2f%\n", miss_rate1);
-  printf("second case: %.2f%\n", miss_rate2);
+
+  float miss_rate = (miss/(float)(miss+hits))*100;
+  printf("miss rate: %.2f%  \n", miss_rate);
+
+
+
 
 }
+
 
